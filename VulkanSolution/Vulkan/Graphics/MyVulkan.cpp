ea63eae8719510100713383cf88cba88ca692b43
Il <rtd990@naver.com>
2021-06-23 10:51:34 +0900

    Finished Chapter5.
    Learn about swapchain, how to create my mode, and cleaning up everything.

--- VulkanSolution/Vulkan/Graphics/MyVulkan.cpp
+++ VulkanSolution/Vulkan/Graphics/MyVulkan.cpp
@@ -39,6 +39,8 @@ namespace MyVulkan
 	void FillBufferWithFloats(VkCommandBuffer cmdBuffer, VkBuffer dstBuffer, VkDeviceSize offset, VkDeviceSize length, const float value);
 
 	void CreateSwapChain();
+
+	void TransitionImageLayout();
 }
 
 void MyVulkan::InitVulkan(const char* appName, uint32_t appVersion)
@@ -490,4 +492,37 @@ void MyVulkan::CreateSwapChain()
 		),
 		"vkGetSwapchainImagesKHR is failed!"
 	);
+}
+
+void MyVulkan::TransitionImageLayout()
+{
+	const VkImageMemoryBarrier barrier =
+	{
+		VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER,			//	sType
+		nullptr, 																							//	pNext
+		VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT,				//	srcAccessMask
+		VK_ACCESS_MEMORY_READ_BIT,										//	dstAccessMask
+		VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL,		//	oldLayout
+		VK_IMAGE_LAYOUT_PRESENT_SRC_KHR,							//	newLayout
+		0,																									//	srcQueueFamilyIndex
+		0,																									//	dstQueueFamilyIndex
+		sourceImage,																				//	image
+		{																									//	subresourceRange
+			VK_IMAGE_ASPECT_COLOR_BIT,										//	aspectMask
+			0,																								//	baseMipLevel
+			1,																								//	levelCount
+			0,																								//	baseArrayLayer
+			1																								//	layerCount
+		}
+	};
+
+	vkCmdPipelineBarrier(
+		cmdBuffer,
+		VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT,
+		VK_PIPELINE_STAGE_BOTTOM_OF_PIPE_BIT,
+		0,
+		0, nullptr,
+		0, nullptr,
+		1, &barrier
+		);
 }
